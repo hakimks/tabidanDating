044dcb5c5857c75818cbc964f8fd96214fd5dd87
hakimks <hakimks@gmail.com>
2019-09-11 17:40:11 +0300

    first commit

---
+++ app/src/main/java/codingwithmitch/com/tabiandating/MainActivity.java
@@ -0,0 +1,48 @@
+package codingwithmitch.com.tabiandating;
+
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import codingwithmitch.com.tabiandating.util.PreferenceKeys;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        isFirstLogin();
+    }
+    
+    private void isFirstLogin(){
+        Log.d(TAG, "isFirstLogin: checking if this is the first login");
+
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstLogin = preferences.getBoolean(PreferenceKeys.FIRST_TIME_LOGIN, true);
+        if (isFirstLogin){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage(getString(R.string.first_time_user_message));
+            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d(TAG, "onClick: closing dialog");
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean(PreferenceKeys.FIRST_TIME_LOGIN, false);
+                    editor.commit();
+                    dialog.dismiss();
+                }
+            });
+
+            alertDialogBuilder.setIcon(R.drawable.tabian_dating);
+            alertDialogBuilder.setTitle(" ");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+    }
+}
