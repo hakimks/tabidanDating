ddecd644a35b0633418f0c06df2f8ea4177ca725
hakimks <hakimks@gmail.com>
2019-09-12 20:25:31 +0300

    Added several files

---
+++ app/src/main/java/codingwithmitch/com/tabiandating/IMainActivity.java
@@ -0,0 +1,12 @@
+package codingwithmitch.com.tabiandating;
+
+import codingwithmitch.com.tabiandating.models.User;
+
+/**
+ * Created by User on 1/24/2018.
+ */
+
+public interface IMainActivity {
+
+    void inflateViewProfileFragment(User user);
+}
--- app/src/main/java/codingwithmitch/com/tabiandating/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/tabiandating/MainActivity.java
@@ -1,48 +1,95 @@
 package codingwithmitch.com.tabiandating;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import codingwithmitch.com.tabiandating.models.User;
 import codingwithmitch.com.tabiandating.util.PreferenceKeys;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IMainActivity{
+
     private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         isFirstLogin();
+        init();
     }
-    
-    private void isFirstLogin(){
-        Log.d(TAG, "isFirstLogin: checking if this is the first login");
+
+    private void init(){
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_content_frame, homeFragment, getString(R.string.tag_fragment_home));
+        transaction.addToBackStack(getString(R.string.tag_fragment_home));
+        transaction.commit();
+    }
+
+    private void isFirstLogin() {
+        Log.d(TAG, "isFirstLogin: checking if this is the first login.");
 
         final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean isFirstLogin = preferences.getBoolean(PreferenceKeys.FIRST_TIME_LOGIN, true);
-        if (isFirstLogin){
+
+        if (isFirstLogin) {
+            Log.d(TAG, "isFirstLogin: launching alert dialog.");
+
+            // launch the info dialog for first-time-users
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setMessage(getString(R.string.first_time_user_message));
-            alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialog, int which) {
+                public void onClick(DialogInterface dialogInterface, int i) {
                     Log.d(TAG, "onClick: closing dialog");
+                    // now that the user has logged in, save it to shared preferences so the dialog won't
+                    // pop up again
                     SharedPreferences.Editor editor = preferences.edit();
                     editor.putBoolean(PreferenceKeys.FIRST_TIME_LOGIN, false);
                     editor.commit();
-                    dialog.dismiss();
+                    dialogInterface.dismiss();
                 }
             });
-
-            alertDialogBuilder.setIcon(R.drawable.tabian_dating);
             alertDialogBuilder.setTitle(" ");
+            alertDialogBuilder.setIcon(R.drawable.tabian_dating);
             AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show();
         }
     }
+
+    @Override
+    public void inflateViewProfileFragment(User user) {
+
+        ViewProfileFragment fragment = new ViewProfileFragment();
+
+        Bundle args = new Bundle();
+        args.putParcelable(getString(R.string.intent_user), user);
+        fragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_content_frame, fragment, getString(R.string.tag_fragment_view_profile));
+        transaction.addToBackStack(getString(R.string.tag_fragment_view_profile));
+        transaction.commit();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
